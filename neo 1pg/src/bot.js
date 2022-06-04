d3c60e01ac912e32ecf07e00fc8f218acc45ccda
rudy <rudychang2006@gmail.com>
2022-06-04 10:12:47 +0800

    fixed dotenv file

--- neo 1pg/src/bot.js
+++ neo 1pg/src/bot.js
@@ -1,3 +1,5 @@
+import { config } from 'dotenv';
+config({ path: '.env' }); 
 import { Client,Intents } from 'discord.js';
 
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] }
@@ -5,13 +7,15 @@ const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MES
 
 bot.on('ready', ()=> console.log('ready'));
 
-bot.login('OTM0MzM1OTkwOTU1NDAxMjE2.GwWi8B.c_2LlVsuduUSzKkva7FPHyzhIverGEyj1jz2gU');
+
 
 bot.on('message', async (msg) =>{
     if(msg.author.bot) return;
 
     await msg.reply('Hi');
 });
+
+bot.login(process.env.BOT_TOKEN);
 
 
 

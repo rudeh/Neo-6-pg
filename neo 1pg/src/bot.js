81db9719f3ea299d0933c1ae6546a2e39f157ba3
rudy <rudychang2006@gmail.com>
2022-06-04 10:39:02 +0800

    added dm feature

--- neo 1pg/src/bot.js
+++ neo 1pg/src/bot.js
@@ -9,13 +9,34 @@ bot.on('ready', ()=> console.log('ready'));
 
 
 
-bot.on('message', async (msg) =>{
-    if(msg.author.bot) return;
+// bot.on('message', async (msg) =>{
+//     if(msg.author.bot) return;
 
-    await msg.reply('Hi');
+//     await msg.reply('Hi');
+// });
+
+bot.on('message', async (msg) =>{
+    if(msg.author.bot) {
+        return
+    }
+    if(msg.content.startsWith("!")){
+        msg.reply("hello world");
+    }
+    if(msg.content.startsWith("!dm")){
+        msg.member.send("how can i help");
+
+    }
+    
+    
 });
 
 bot.login(process.env.BOT_TOKEN);
+
+
+
+
+
+
 
 
 
